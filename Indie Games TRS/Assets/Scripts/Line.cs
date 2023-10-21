6ddf53db8411f2ad283f9b416a38f0d1acbb64d0
guillemal <guillem.alqueza@estudiantat.upc.edu>
2023-10-22 00:58:48 +0200

    line

---
+++ Indie Games TRS/Assets/Scripts/Line.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] private float animationDuration = 5f;
+
+    private LineRenderer lineRenderer;
+    private Vector3[] linePoints;
+    private int pointsCount;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        pointsCount = lineRenderer.positionCount;
+        linePoints = new Vector3[pointsCount];
+        for (int i = 0; i < pointsCount; i++)
+        {
+            linePoints[i] = lineRenderer.GetPosition(i);
+        }
+
+        StartCoroutine(AnimateLine());
+    }
+
+    private IEnumerator AnimateLine()
+    {
+        float segmentDuration = animationDuration / pointsCount;
+
+        for (int i = 0; i < pointsCount - 1; i++)
+        {
+            float startTime = Time.time;
+
+            Vector3 startPosition = linePoints[i];
+            Vector3 endPosition = linePoints[i + 1];
+
+            Vector3 pos = startPosition;
+            while (pos != endPosition)
+            {
+                float t = (Time.time - startTime) / segmentDuration;
+                pos = Vector3.Lerp(startPosition, endPosition, t);
+
+                for (int j = i + 1; j < pointsCount; j++)
+                    lineRenderer.SetPosition(j, pos);
+
+                yield return null;
+            }
+        }
+    }
+}
